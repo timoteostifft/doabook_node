b0f7c1bea8da34f7aeda433cbc623cc77645aa51
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-20 10:06:38 -0300

    feature: create user entity

---
+++ src/modules/users/infra/prisma/entities/User.ts
@@ -0,0 +1,18 @@
+import { v4 as uuidV4 } from 'uuid'
+
+class User {
+  id: string
+  name: string
+  username: string 
+  email: string 
+  password: string 
+  isAdmin: boolean
+
+  constructor(){
+    if(!this.id){
+      this.id = uuidV4()
+    }
+  }
+}
+
+export { User }
