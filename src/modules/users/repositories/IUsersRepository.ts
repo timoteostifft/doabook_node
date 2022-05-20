0ad7ca228c166b6ff813df3374f767febf9dd86b
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-20 12:55:10 -0300

    refactor: removed unused list by username method

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -4,9 +4,8 @@ import { User } from "../infra/prisma/entities/User"
 
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<User>
-  update(data: IUpdateUserDTO): Promise<User>
+  update({ id, data }: IUpdateUserDTO): Promise<User>
   list(): Promise<User[]>
-  findByUsername(username: string): Promise<User | null>
   findByEmail(email: string): Promise<User | null>
   findById(id: string): Promise<User | null>
 }
