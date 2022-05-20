b7df89a030f84f73ed45bfa4146e7bab4677bbe3
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-20 12:57:14 -0300

    feature: create update user route

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 import { CreateUserController } from '../../../../modules/users/useCases/createUser/CreateUserController'
 import { ListUsersController } from '../../../../modules/users/useCases/listUsers/ListUsersController'
+import { UpdateUserController } from '../../../../modules/users/useCases/updateUser/UpdateUserController'
 
 const usersRoutes = Router()
 
 const listUsersController = new ListUsersController()
 const createUserController = new CreateUserController()
+const updateUserController = new UpdateUserController()
 
 usersRoutes.get('/', listUsersController.handle)
 usersRoutes.post('/', createUserController.handle)
+usersRoutes.post('/update/:id', updateUserController.handle)
 
 export { usersRoutes }
