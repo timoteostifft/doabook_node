8206c87af4158c6bad4a462d7b4714b8efa01ddf
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-20 11:53:24 -0300

    feature: create list method

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -5,6 +5,7 @@ import { User } from "../infra/prisma/entities/User"
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<User>
   update(data: IUpdateUserDTO): Promise<User>
+  list(): Promise<User[]>
   findByUsername(username: string): Promise<User | null>
   findByEmail(email: string): Promise<User | null>
   findById(id: string): Promise<User | null>
