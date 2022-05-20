04bed626d3bbab483fe80433f7df88a2d4905318
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-20 11:51:53 -0300

    feature: create list users use case

---
+++ src/modules/users/useCases/listUsers/ListUsersUseCase.ts
@@ -0,0 +1,18 @@
+import { inject, injectable } from 'tsyringe'
+import { User } from '../../infra/prisma/entities/User'
+import { IUsersRepository } from '../../repositories/IUsersRepository'
+
+@injectable()
+class ListUsersUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute(): Promise<User[]>{
+    const users = await this.usersRepository.list()
+    return users
+  }
+}
+
+export { ListUsersUseCase }
