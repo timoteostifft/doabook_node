2a0d7338f81ebdb0d831aaa3bf427cb976901580
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-20 11:52:34 -0300

    feature: create list users route

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { CreateUserController } from '../../../../modules/users/useCases/createUser/CreateUserController'
+import { ListUsersController } from '../../../../modules/users/useCases/listUsers/ListUsersController'
 
 const usersRoutes = Router()
 
+const listUsersController = new ListUsersController()
 const createUserController = new CreateUserController()
 
+usersRoutes.get('/', listUsersController.handle)
 usersRoutes.post('/', createUserController.handle)
 
 export { usersRoutes }
