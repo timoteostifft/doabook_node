74f140788098c61156ae93f605dac7b3479570c6
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-21 03:54:33 -0300

    feature: implement institutions routes on server

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { usersRoutes } from './users.routes'
 import { authenticateRoutes } from './authenticate.routes'
+import { institutionsRoutes } from './institutions.routes'
 
 const router = Router()
 
 router.use("/users", usersRoutes)
 router.use("/", authenticateRoutes)
+router.use("/institutions", institutionsRoutes)
 
 export { router }
