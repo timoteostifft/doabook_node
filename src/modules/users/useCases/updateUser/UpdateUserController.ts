8fc933fdf22b19f20915b1fb01cd0fe97ae4aa77
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-21 02:13:41 -0300

    refactor: update id param

--- src/modules/users/useCases/updateUser/UpdateUserController.ts
+++ src/modules/users/useCases/updateUser/UpdateUserController.ts
@@ -7,7 +7,7 @@ class UpdateUserController {
 
   async handle(request: Request, response: Response): Promise<Response>{
 
-    const { id } = request.params
+    const { id } = request.user
 
     const data = request.body
 
