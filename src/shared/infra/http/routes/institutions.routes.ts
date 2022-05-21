f76affb0605030e987fc625f0255bbb1f880879e
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-21 04:50:50 -0300

    feature: create list institutions route

--- src/shared/infra/http/routes/institutions.routes.ts
+++ src/shared/infra/http/routes/institutions.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 import { CreateInstitutionController } from '../../../../modules/institutions/useCases/createInstitution/CreateInstitutionController'
+import { ListInstitutionsController } from '../../../../modules/institutions/useCases/listInstitutions/ListInstitutionsController'
 import { UpdateInstitutionAdminController } from '../../../../modules/institutions/useCases/updateInstitutionAdmin/UpdateInstitutionAdminController'
 
 const institutionsRoutes = Router()
 
 const createInstitutionController = new CreateInstitutionController()
 const updateInstitutionAdminController = new UpdateInstitutionAdminController()
+const listInstitutionsController = new ListInstitutionsController()
 
 institutionsRoutes.post('/', createInstitutionController.handle)
 institutionsRoutes.patch('/:id', updateInstitutionAdminController.handle)
+institutionsRoutes.get("/", listInstitutionsController.handle)
 
 export { institutionsRoutes }
