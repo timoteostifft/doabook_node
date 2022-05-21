ac7b8d7631f8d795681db7b7bd260c3ee2670c1d
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-21 03:53:54 -0300

    feature: create institutions repository container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,18 @@
 import { container } from "tsyringe";
+
 import { IUsersRepository } from '../../modules/users/repositories/IUsersRepository'
 import { UsersRepository } from "../../modules/users/infra/prisma/repositories/UsersRepository";
+
+import { IInstitutionsRepository } from '../../modules/institutions/repositories/IInstitutionsRepository'
+import { InstitutionsRepository } from '../../modules/institutions/infra/prisma/repositories/InstitutionsRepository'
 
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
+)
+
+container.registerSingleton<IInstitutionsRepository>(
+  "InstitutionsRepository",
+  InstitutionsRepository
 )
