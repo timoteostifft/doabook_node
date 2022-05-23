7ad9494889f9c0e48b956efd75d80200e3f282e0
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-23 02:59:34 -0300

    feature: create update book method

--- src/modules/books/repositories/IBooksRepository.ts
+++ src/modules/books/repositories/IBooksRepository.ts
@@ -1,10 +1,13 @@
 import { ICreateBookDTO } from "../dtos/ICreateBookDTO"
+import { IUpdateBookDTO } from "../dtos/IUpdateBookDTO"
 import { Book } from "../infra/prisma/entities/Book"
 
 interface IBooksRepository {
   create(data: ICreateBookDTO): Promise<Book>
   increment(id: string): Promise<Book>
+  update({ id, data }: IUpdateBookDTO): Promise<Book>
   findExist(name: string, edition: number, condition: string): Promise<Book | null>
+  findById(id: string): Promise<Book | null>
 }
 
 export { IBooksRepository }
