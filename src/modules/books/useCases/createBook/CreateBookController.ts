6510a1ddbe229889a488c82e78ec371cb725e3d6
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-23 00:35:45 -0300

    feature: create create book controller

---
+++ src/modules/books/useCases/createBook/CreateBookController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+// import { Book } from '../infra/prisma/entities/Book'
+import { CreateBookUseCase } from './CreateBookUseCase'
+
+
+class CreateBookController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { 
+      name, 
+      edition, 
+      year,
+      launch,
+      condition,
+      quantity,
+      institution_id } = request.body
+
+      const createBookUseCase = container.resolve(CreateBookUseCase)
+
+      await createBookUseCase.execute({
+        name, 
+        edition, 
+        year,
+        launch,
+        condition,
+        quantity,
+        institution_id
+      })
+
+      return response.status(201).send()
+  }
+}
+
+export { CreateBookController }
