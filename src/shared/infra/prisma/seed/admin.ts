22141dbba7dc2a32c0b6f793197e157cf418d99d
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-23 00:38:15 -0300

    refactor: update admin seed

--- src/shared/infra/prisma/seed/admin.ts
+++ src/shared/infra/prisma/seed/admin.ts
@@ -1,9 +1,18 @@
-const admin = {
-  name: "admin",
-  username: "admin",
-  email: "admin@doabook.com.br",
-  password: "admin",
-  isAdmin: true
+import { v4 as uuidV4 } from 'uuid'
+import { hash }  from 'bcrypt'
+
+// const encrypted = async () => {
+//   return await hash('admin', 8)
+// }
+
+const admin = async () => {
+  return {
+    id: uuidV4(),
+    name: "admin",
+    email: "admin@doabook.com.br",
+    password: await hash('admin', 8),
+    isAdmin: true
+  }
 }
 
 export { admin }
