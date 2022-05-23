7d90a6de85df0c5d78acc9f61be8a7726e518b13
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-23 00:37:57 -0300

    feature: books route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import { usersRoutes } from './users.routes'
 import { authenticateRoutes } from './authenticate.routes'
 import { institutionsRoutes } from './institutions.routes'
+import { booksRoutes } from './books.routes'
 
 const router = Router()
 
 router.use("/users", usersRoutes)
 router.use("/", authenticateRoutes)
 router.use("/institutions", institutionsRoutes)
+router.use("/institutions/books", booksRoutes)
 
 export { router }
