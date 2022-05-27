b8ab6ae59ffb236f15aa13f6f7b123d0a6a903d2
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-27 13:03:34 -0300

    feature: implement cors

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,14 +1,25 @@
+import { Request, Response, NextFunction } from "express"
 import { Router } from 'express'
 import { usersRoutes } from './users.routes'
 import { authenticateRoutes } from './authenticate.routes'
 import { institutionsRoutes } from './institutions.routes'
 import { booksRoutes } from './books.routes'
+import Cors from 'cors'
 
 const router = Router()
+
+const cors = Cors()
+
+router.use(cors)
+
+router.use((request: Request, response: Response, next: NextFunction) => {
+  response.header("Access-Control-Allow-Origin", "*")
+  next();
+});
 
 router.use("/users", usersRoutes)
 router.use("/", authenticateRoutes)
 router.use("/institutions", institutionsRoutes)
-router.use("/institutions/books", booksRoutes)
+router.use("/books", booksRoutes)
 
 export { router }
