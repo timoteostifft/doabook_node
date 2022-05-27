52b51e4266412b8e72e1d854d5eccf6a77445dff
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-27 13:03:20 -0300

    feature: create list by id route

--- src/shared/infra/http/routes/books.routes.ts
+++ src/shared/infra/http/routes/books.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 import { CreateBookController } from '../../../../modules/books/useCases/createBook/CreateBookController'
+import { ListBooksController } from '../../../../modules/books/useCases/listBooks/listBooksController'
 import { UpdateBookController } from '../../../../modules/books/useCases/updateBook/UpdateBookController'
 
 const booksRoutes = Router()
 
 const createBookController = new CreateBookController()
 const updateBookController = new UpdateBookController()
+const listBooksController = new ListBooksController()
 
 booksRoutes.post("/", createBookController.handle)
 booksRoutes.patch("/:id", updateBookController.handle)
+booksRoutes.get('/:institution_id', listBooksController.handle)
 
 export { booksRoutes }
