44049cbd1abc3617ffa0e7e2580ca11b13f296e4
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-27 13:04:37 -0300

    feature: create list user by id route

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { CreateUserController } from '../../../../modules/users/useCases/createUser/CreateUserController'
+import { ListUserByIdController } from '../../../../modules/users/useCases/listUserById/ListUserByIdController'
 import { ListUsersController } from '../../../../modules/users/useCases/listUsers/ListUsersController'
 import { UpdateUserController } from '../../../../modules/users/useCases/updateUser/UpdateUserController'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
@@ -9,9 +10,11 @@ const usersRoutes = Router()
 const listUsersController = new ListUsersController()
 const createUserController = new CreateUserController()
 const updateUserController = new UpdateUserController()
+const listUserByIdController = new ListUserByIdController()
 
 usersRoutes.get('/', listUsersController.handle)
 usersRoutes.post('/', createUserController.handle)
 usersRoutes.patch('/update/', ensureAuthenticated, updateUserController.handle)
+usersRoutes.get('/:id', ensureAuthenticated, listUserByIdController.handle)
 
 export { usersRoutes }
